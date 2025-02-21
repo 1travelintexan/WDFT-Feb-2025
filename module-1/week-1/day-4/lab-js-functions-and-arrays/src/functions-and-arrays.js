e1a6660e6fff689c9ab8e85b445d7ee7d689e14d
Joshua <horheyinc@gmail.com>
2025-02-21 10:08:18 +0100

    lab solution

--- module-1/week-1/day-4/lab-js-functions-and-arrays/src/functions-and-arrays.js
+++ module-1/week-1/day-4/lab-js-functions-and-arrays/src/functions-and-arrays.js
@@ -21,17 +21,39 @@ const words = [
   "crackpot",
 ];
 
-function findLongestWord() {}
+function findLongestWord(arrayOfWords) {
+  if (arrayOfWords.length === 0) return null;
+  let longestWord = "";
+  for (let i = 0; i < arrayOfWords.length; i++) {
+    const currentWord = arrayOfWords[i];
+    if (currentWord.length > longestWord.length) {
+      longestWord = currentWord;
+    }
+  }
+  return longestWord;
+}
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arrOfNums) {
+  let sum = 0;
+  for (let i = 0; i < arrOfNums.length; i++) {
+    const currentNumber = arrOfNums[i];
+    sum = sum + currentNumber;
+  }
+  return sum;
+}
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(array) {
+  if (array.length === 0) return 0;
+  const averageSum = sumNumbers(array);
+  const avg = averageSum / array.length;
+  return avg;
+}
 
 // Iteration 5 | Find Elements
 const words2 = [
@@ -45,4 +67,17 @@ const words2 = [
   "disobedience",
 ];
 
-function doesWordExist() {}
+function doesWordExist(arrayOfWords, aWord) {
+  if (arrayOfWords.length === 0) return null;
+  //   let isInsideArray = false;
+  //   for (let i = 0; i < arrayOfWords.length; i++) {
+  //     const currentWord = arrayOfWords[i];
+  //     if (currentWord === aWord) {
+  //       isInsideArray = true;
+  //     }
+  //   }
+  //   return isInsideArray;
+
+  //oneline version
+  return arrayOfWords.includes(aWord);
+}
