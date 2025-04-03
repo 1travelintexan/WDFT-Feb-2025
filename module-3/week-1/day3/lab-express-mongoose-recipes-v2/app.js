87c66fa0ef69b4b4980d65786f74bc06a60a56bd
Joshua <horheyinc@gmail.com>
2025-04-03 10:19:14 +0200

    lab solution

--- module-3/week-1/day3/lab-express-mongoose-recipes-v2/app.js
+++ module-3/week-1/day3/lab-express-mongoose-recipes-v2/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const RecipeModel = require("./models/Recipe.model");
 const app = express();
 
 // MIDDLEWARE
@@ -28,19 +29,76 @@ app.get("/", (req, res) => {
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
+app.post("/recipes", async (req, res) => {
+  try {
+    console.log(req.body);
+    const createdRecipe = await RecipeModel.create({
+      ...req.body,
+      title: req.body.name,
+      level: req.body.difficulty,
+    });
+    console.log("Recipe Created! ", createdRecipe);
+    res.status(201).json(createdRecipe);
+  } catch (error) {
+    console.log(error);
+    // if(error.message === 'Duplicate Key error')
+    res.status(500).json({ errorMessage: "Problem creating the recipe" });
+  }
+});
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
+app.get("/recipes", (req, res) => {
+  RecipeModel.find()
+    .then((allRecipes) => {
+      console.log("Recipes ", allRecipes);
+      res.status(200).json(allRecipes);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "Problem creating the recipe" });
+    });
+});
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
-
+app.get("/recipe/:id", (req, res) => {
+  RecipeModel.findById(req.params.id)
+    .then((oneRecipe) => {
+      console.log("Recipe ", oneRecipe);
+      res.status(200).json(oneRecipe);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "Problem finding the recipe" });
+    });
+});
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
-
+app.patch("/recipe/:pizzaId", (req, res) => {
+  RecipeModel.findByIdAndUpdate(req.params.pizzaId, req.body, { new: true })
+    .then((updatedRecipe) => {
+      console.log("recipe updated", updatedRecipe);
+      res.status(425).json(updatedRecipe);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ errorMessage: "Problem updating the recipe" });
+    });
+});
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
-
+app.delete("/recipe/:recipeId", async (req, res) => {
+  //destructure the params
+  const { recipeId } = req.params;
+  try {
+    const deletedRecipe = await RecipeModel.findByIdAndDelete(recipeId);
+    console.log("recipe deleted", deletedRecipe);
+    res.status(200).json(deletedRecipe);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ errorMessage: "Problem deleting the recipe" });
+  }
+});
 // Start the server
 app.listen(3000, () => console.log("My first app listening on port 3000!"));
 
--- module-3/week-1/day3/lab-express-mongoose-recipes-v2/models/Recipe.model.js
+++ module-3/week-1/day3/lab-express-mongoose-recipes-v2/models/Recipe.model.js
@@ -1,1 +1,39 @@
-// Your code here ...
+const { Schema, model } = require("mongoose");
+
+const recipeSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  instructions: {
+    type: String,
+    required: true,
+  },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "Ultra Pro Chef"],
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  ingredients: {
+    type: [String],
+  },
+  duration: {
+    type: Number,
+    min: 0,
+  },
+  isArchived: {
+    type: Boolean,
+    default: false,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const RecipeModel = model("recipe", recipeSchema);
+module.exports = RecipeModel;
